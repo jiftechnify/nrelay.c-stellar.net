bc2d27afe3186421fa6331d0fa5c32f060ae0842
jiftechnify <jiftech.stlfy@gmail.com>
2024-02-05 22:12:07 +0900

    introduce cache to follow check API client

--- sifter/api/follow_check.go
+++ sifter/api/follow_check.go
@@ -3,23 +3,36 @@ package api
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
+	"time"
 )
 
 type Client struct {
 	baseURL string
+	cache   map[string]time.Time
 }
 
 func NewClient(baseURL string) *Client {
-	return &Client{baseURL: strings.TrimSuffix(baseURL, "/")}
+	return &Client{
+		baseURL: strings.TrimSuffix(baseURL, "/"),
+		cache:   make(map[string]time.Time),
+	}
 }
 
 type isFollowerResp struct {
 	IsFollower bool `json:"isFollower"`
 }
 
+const cacheTTL = 1 * time.Hour
+
 func (c *Client) IsFollower(author string) (bool, error) {
+	if t, ok := c.cache[author]; ok && time.Since(t) < cacheTTL {
+		log.Printf("is_follower cache hit (pubkey: %s)", author)
+		return true, nil
+	}
+
 	apiURL := fmt.Sprintf("%s/is_follower?pubkey=%s", c.baseURL, author)
 	resp, err := http.Get(apiURL)
 	if err != nil {
@@ -30,6 +43,13 @@ func (c *Client) IsFollower(author string) (bool, error) {
 	var r isFollowerResp
 	if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
 		return false, fmt.Errorf("failed to decode response from API server: %w", err)
+	}
+
+	// update cache
+	if r.IsFollower {
+		c.cache[author] = time.Now()
+	} else {
+		delete(c.cache, author)
 	}
 	return r.IsFollower, nil
 }
